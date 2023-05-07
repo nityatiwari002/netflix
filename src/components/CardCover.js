18bd4d7604d074cb69e06931c9aff0988e68e618
Nitya Tiwari <nityasdl002@gmail.com>
2023-05-07 17:33:40 +0530

    modifications

--- src/components/CardCover.js
+++ src/components/CardCover.js
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from 'react';
-import '../components-style/CardCover.css';
-import axios from '../utilities/axios';
+import React, { useEffect, useState } from "react";
+import "../components-style/CardCover.css";
+import axios from "../utilities/axios";
 
-const imgUrl = "https://image.tmdb.org/t/p/original"
+const imgUrl = "https://image.tmdb.org/t/p/original";
 
 function CardCover(fetchURL) {
     const [movies, setMovies] = useState([]);
@@ -13,7 +13,7 @@ function CardCover(fetchURL) {
             const req = await axios.get(url);
             // console.log(url);
             setMovies(req.data.results);
-            console.log(req);
+            // console.log(req);
             return url;
         }
         getData();
@@ -24,18 +24,21 @@ function CardCover(fetchURL) {
     // console.log(fetchURL);
     let thumb = fetchURL.thumb;
     return (
-        <div className='cardcover'>
-            {movies.map(movie => (
-                
-                <img 
+        <div className="cardcover">
+            {movies.map((movie) => (
+                <img
                     key={movie.id}
-                    className={thumb==="Y" ? `cardimg thumbimg` : `cardimg`}
-                    src={thumb === "Y" ? `${imgUrl}${movie.backdrop_path}` : `${imgUrl}${movie.poster_path}`} alt={movie.title}
+                    className={thumb === "Y" ? `cardimg thumbimg` : `cardimg`}
+                    src={
+                        thumb === "Y"
+                            ? `${imgUrl}${movie.backdrop_path}`
+                            : `${imgUrl}${movie.poster_path}`
+                    }
+                    alt={movie.title}
                 />
             ))}
-            
         </div>
-    )
-};
+    );
+}
 
 export default CardCover;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,38 @@
-import '../components-style/Header.css';
+import { useEffect, useState } from "react";
+import "../components-style/Header.css";
+import Description from "./Description";
+import Title from "./Title";
+import Buttons from '../components/Buttons';
+import axios from "../utilities/axios";
+import requests from "../utilities/requests";
+
+const imgUrl = "https://image.tmdb.org/t/p/original";
 
 function Header() {
-  return (
-    <div className="Header">
-      <div className='overlay'>
-        
-      </div>
-    </div>
-  );
+    const urls = Object.values(requests);
+    const url = urls[Math.floor(Math.random() * urls.length)];
+    const [movie, setMovie] = useState([]);
+    useEffect(() => {
+        async function getMovie() {
+            const req = await axios.get(url);
+            let x = Math.floor(Math.random() * 100) % 20;
+            setMovie(req.data.results[x]);
+            return url;
+        }
+        getMovie();
+    }, [url]);
+    return (
+        <header
+            className="Header"
+            style={{ backgroundImage: `url(${imgUrl}${movie.backdrop_path})` }}
+        >
+            <div className="overlay">
+                <Title content={movie.title} />
+                <Buttons />
+                <Description content={movie.overview} />
+            </div>
+        </header>
+    );
 }
 
 export default Header;
